1ddb444268495e2d68c8f0b1ba47c13d6a949b89
Kevin Gibbs <kevingibbs009@gmail.com>
2026-02-05 17:52:52 -0600

    engine setup

--- blackjack_engine.c
+++ blackjack_engine.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #define DECK_SIZE 52
 
-void create_deck(uint8_t **deck);
+typedef struct player {
+    int money;
+    int hand;
+} Player;
 
+typedef struct dealer {
+    int showing;
+    int hand;
+} Dealer;
+
+void create_deck(uint8_t **deck);
+void engine(uint8_t *cards, Player *player, Dealer *dealer);
+
+// Currently main for testing, will be turning into setup(bool automate)
+// so that other programs can call to start a game
 int main(int argc, char **argv) {
     uint8_t *cards = NULL;
     create_deck(&cards);
@@ -13,17 +27,41 @@ int main(int argc, char **argv) {
         fprintf(stderr, "An error occurred in deck creation\n");
         return -1;
     }
-    
+
+    // Create player
+    Player *player = (Player *) calloc(1, sizeof(Player));
+    player->money = 1000;
+
+    // Create dealer
+    Dealer *dealer = (Dealer *) calloc(1, sizeof(Dealer));
+
+    engine(cards, player, dealer);
+
     free(cards);
+    free(player); // No internal pointers
+    free(dealer); // No internal pointers
     return 0;
 }
 
-void create_deck(uint8_t **deck) {
-    *deck = malloc(sizeof(uint8_t)*DECK_SIZE);
+// The main function that drives the game
+static void engine(uint8_t *cards, Player *player, Dealer *dealer) {
+
+}
+
+static void create_deck(uint8_t **deck) {
+    // May want to just malloc instead of calloc since deck values are immediately set.
+    *deck = calloc(DECK_SIZE, sizeof(uint8_t));
+    // *deck = malloc(sizeof(uint8_t)*DECK_SIZE);
+
     for (int i = 0; i < DECK_SIZE; i++) {
         (*deck)[i] = i & 0xFF;
     }
-    for (int i = 0; i < DECK_SIZE; i++) {
-        printf("%d%s", (*deck)[i], "\n");
-    }
+    // for (int i = 0; i < DECK_SIZE; i++) {
+    //     printf("%d%s", (*deck)[i], "\n");
+    // }
+}
+
+// Shuffles the deck of cards into a random order
+static void shuffle(uint8_t *cards) {
+
 }
